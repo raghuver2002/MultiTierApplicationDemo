8df8e10b475fae5dadd175114c781bdb8f9ec263
Raghvendra Nath Pandey <raghvendranath.pandey@gmail.com>
2025-05-15 02:49:02 +0530

    backend update app.py

--- backend/app.py
+++ backend/app.py
@@ -25,5 +25,25 @@ def db_test():
     except Exception as e:
         return f"DB connection failed: {str(e)}"
 
+
+@app.route("/add-sample")
+def add_sample():
+    try:
+        conn = psycopg2.connect(
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB_NAME'],
+            user=os.environ['DB_USER'],
+            password=os.environ['DB_PASS']
+        )
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS test_table (id SERIAL PRIMARY KEY, name TEXT);")
+        cur.execute("INSERT INTO test_table (name) VALUES ('DockerUser') RETURNING id;")
+        conn.commit()
+        cur.close()
+        return "Inserted into DB!"
+    except Exception as e:
+        return str(e)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
